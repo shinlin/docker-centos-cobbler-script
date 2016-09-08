f4d71aa72f24908b02c3d2a32b6b9f47f195718f
liumk <mkliu28@gmail.com>
2016-09-08 21:37:49 +0800

    add1

--- scripts/add-systems.sh
+++ scripts/add-systems.sh
@@ -1,40 +1,87 @@
+#!/bin/bash
 
-cobbler system remove --name=system-thinkserver-PC0EQQ67 
+function add_single_system() {
+# add one system with multiple NICs
+# input parameters format:
+# system_name profile host_name MAC0 IP0 MAC1 IP1 MAC2 IP2 MAC3 IP3 NETMASK GATEWAY
+    system_name=$1
+    profile=$2 
+    host_name=$3 
+    MAC0=$4
+    IP0=$5 
+    MAC1=$6 
+    IP1=$7
+    MAC2=$8 
+    IP2=$9 
+    MAC3=$10
+    IP3=$11
+    NETMASK=$12
+    GATEWAY=$13
 
-cobbler system add \
---name=system-thinkserver-PC0EQQ67 \
---profile=centos72-1511-istack-thinkserver \
---hostname=thinkserver-PC0EQQ67 \
---name-servers=203.95.1.2 \
---interface=eth0 \
---mac=A0:36:9F:9B:34:C6 \
---static=1 \
---ip-address=192.168.11.139 \
---subnet=255.255.255.0
+    cobbler system remove --name=${system_name}
+    cobbler system add \
+    --name=${system_name} \
+    --profile=${profile} \
+    --hostname=${host_name} \
+    --name-servers=203.95.1.2 \
+    --interface=eth0 \
+    --mac=${MAC0} \
+    --static=1 \
+    --ip-address=${IP0} \
+    --subnet=${NETMASK}
 
-cobbler system edit \
---name=system-thinkserver-PC0EQQ67 \
---interface=eth1 \
---mac=A0:36:9F:9B:34:C7 \
---static=1 \
---ip-address=192.168.12.139 \
---subnet=255.255.255.0
+    cobbler system edit \
+    --name=${system_name} \
+    --interface=eth1 \
+    --mac=${MAC1} \
+    --static=1 \
+    --ip-address=${IP1} \
+    --subnet=${NETMASK}
 
-cobbler system edit \
---name=system-thinkserver-PC0EQQ67 \
---interface=eth2 \
---mac=68:05:CA:31:D5:CD \
---static=1 \
---management=true \
---if-gateway=192.168.1.1 \
---ip-address=192.168.1.139 \
---subnet=255.255.255.0
+    cobbler system edit \
+    --name=${system_name} \
+    --interface=eth2 \
+    --mac=${MAC2} \
+    --static=1 \
+    --management=true \
+    --if-gateway=${GATEWAY} \
+    --ip-address=${IP2}\
+    --subnet=${NETMASK}
 
-cobbler system edit \
---name=system-thinkserver-PC0EQQ67 \
---interface=eth3 \
---mac=68:05:CA:31:D5:CC \
---static=1 \
---ip-address=192.168.2.139 \
---subnet=255.255.255.0
+    cobbler system edit \
+    --name=${system_name} \
+    --interface=eth3 \
+    --mac=${MAC3} \
+    --static=1 \
+    --ip-address=${IP3} \
+    --subnet=${NETMASK}
+
+}
+
+#for line in `cat RD650NIC-MAC.txt`
+#do
+#    
+#done
+
+cat RD650NIC-MAC.txt | while read LINE 
+do        
+    system_name=`echo $LINE|cut -d ' ' -f 1`
+    profile=centos72-1511-istack-thinkserver.ks
+    host_name="thinkserver-${system_name}"
+    NETMASK=255.255.255.0
+    GATEWAY=192.168.1.1
+
+    MAC0=`echo $LINE|cut -d ' ' -f 2`
+    MAC1=`echo $LINE|cut -d ' ' -f 3`
+    MAC2=`echo $LINE|cut -d ' ' -f 4`
+    MAC3=`echo $LINE|cut -d ' ' -f 5`
+
+    IP0=`echo $LINE|cut -d ' ' -f 6`
+    IP1=`echo $LINE|cut -d ' ' -f 7`
+    IP2=`echo $LINE|cut -d ' ' -f 8`
+    IP3=`echo $LINE|cut -d ' ' -f 9`
+        
+    add_single_system ${system_name} ${profile } ${host_name}  ${MAC0} ${IP0} ${MAC1} ${IP1} ${MAC2} ${IP2} ${MAC3} ${IP3} ${NETMASK} ${GATEWAY}
+done
+
 
